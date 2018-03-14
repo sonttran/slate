7991a6aca1b5b5970000a852dbd758b06433167a
Son Tran <coded.fire@gmail.com>
2018-03-13 23:41:49 -0700

    Update .slate.js

--- .slate.js
+++ .slate.js
@@ -31,9 +31,37 @@ var bottomLeftTwoThird = slate.operation("move", {
     "width" : "screenSizeX*2/3",
     "height" : "screenSizeY/3"
 });
+var halfLeft = slate.operation("move", {
+    "x" : "screenOriginX",
+    "y" : "screenOriginY",
+    "width" : "screenSizeX/2",
+    "height" : "screenSizeY"
+});
+var halfRight = slate.operation("move", {
+    "x" : screenWidth / 2,
+    "y" : "screenOriginY",
+    "width" : "screenSizeX/2",
+    "height" : "screenSizeY"
+});
+var halfTop = slate.operation("move", {
+    "x" : "screenOriginX",
+    "y" : "screenOriginY",
+    "width" : "screenSizeX",
+    "height" : "screenSizeY/2"
+});
+var halfBottom = slate.operation("move", {
+    "x" : "screenOriginX",
+    "y" : screenHeight / 2, 
+    "width" : "screenSizeX",
+    "height" : "screenSizeY/2"
+});
 
 slate.bind("m:ctrl,alt,cmd", function() { fullscreen.run() });
 slate.bind("up:ctrl,alt,shift", function() { topRightTwoThird.run() });
 slate.bind("left:ctrl,alt,shift", function() { topRightOneThird.run() });
 slate.bind("right:ctrl,alt,shift", function() { bottomRightTwoThird.run() });
 slate.bind("down:ctrl,alt,shift", function() { bottomLeftTwoThird.run() });
+slate.bind("left:ctrl,alt,cmd", function() { halfLeft.run() });
+slate.bind("right:ctrl,alt,cmd", function() { halfRight.run() });
+slate.bind("up:ctrl,alt,cmd", function() { halfTop.run() });
+slate.bind("down:ctrl,alt,cmd", function() { halfBottom.run() });

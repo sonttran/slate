5d7992d9e1b8bad72a4af06a4c7b1040a5ff5a40
Son Tran <coded.fire@gmail.com>
2018-03-21 15:29:45 -0700

    Update .slate.js

--- .slate.js
+++ .slate.js
@@ -13,7 +13,7 @@ var topRightTwoThird = slate.operation("move", {
     "width" : "screenSizeX*2/3",
     "height" : "screenSizeY*2/3"
 });
-var topRightOneThird = slate.operation("move", {
+var topLeftOneThird = slate.operation("move", {
     "x" : "screenOriginX",
     "y" : "screenOriginY",
     "width" : "screenSizeX/3",
@@ -55,15 +55,29 @@ var halfBottom = slate.operation("move", {
     "width" : "screenSizeX",
     "height" : "screenSizeY/2"
 });
+var rightTwoThird = slate.operation("move", {
+    "x" : screenWidth / 3,
+    "y" : "screenOriginY",
+    "width" : "screenSizeX*2/3",
+    "height" : "screenSizeY"
+});
+var leftOneThird = slate.operation("move", {
+    "x" : "screenOriginX",
+    "y" : "screenOriginY",
+    "width" : "screenSizeX/3",
+    "height" : "screenSizeY"
+});
 
 slate.bindAll({
-    "m:ctrl,alt,cmd"        : fullscreen,
-    "up:ctrl,alt,shift"     : topRightTwoThird,
-    "left:ctrl,alt,shift"   : topRightOneThird,
-    "right:ctrl,alt,shift"  : bottomRightTwoThird,
-    "down:ctrl,alt,shift"   : bottomLeftTwoThird,
-    "left:ctrl,alt,cmd"     : halfLeft,
-    "right:ctrl,alt,cmd"    : halfRight,
-    "up:ctrl,alt,cmd"       : halfTop,
-    "down:ctrl,alt,cmd"     : halfBottom
+    "m:ctrl,alt,cmd"                : fullscreen,
+    "up:ctrl,alt,shift"             : topRightTwoThird,
+    "left:ctrl,alt,shift"           : topLeftOneThird,
+    "right:ctrl,alt,shift"          : bottomRightTwoThird,
+    "down:ctrl,alt,shift"           : bottomLeftTwoThird,
+    "left:ctrl,alt,cmd"             : halfLeft,
+    "right:ctrl,alt,cmd"            : halfRight,
+    "up:ctrl,alt,cmd"               : halfTop,
+    "down:ctrl,alt,cmd"             : halfBottom,
+    "left:ctrl,alt,cmd,shift"       : leftOneThird,
+    "right:ctrl,alt,cmd,shift"      : rightTwoThird,
 })
